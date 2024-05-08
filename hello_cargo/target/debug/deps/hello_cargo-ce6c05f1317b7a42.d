8c54d2f8aef1a3cb9b66eead9f1ed13326d428e0
praniil <pparajuli15@gmail.com>
2024-05-08 18:30:53 +0545

    feat: implemented cargo in the rust project

---
+++ hello_cargo/target/debug/deps/hello_cargo-ce6c05f1317b7a42.d
@@ -0,0 +1,5 @@
+/home/pranil/rustProjects/learningRust/hello_cargo/target/debug/deps/hello_cargo-ce6c05f1317b7a42: src/main.rs
+
+/home/pranil/rustProjects/learningRust/hello_cargo/target/debug/deps/hello_cargo-ce6c05f1317b7a42.d: src/main.rs
+
+src/main.rs:
---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/home/pranil/rustProjects/learningRust/hello_cargo/target/debug/hello_cargo: /home/pranil/rustProjects/learningRust/hello_cargo/src/main.rs
