d92fc78e9c67a6b328b95c16f705e582b440a194
praniil <pparajuli15@gmail.com>
2024-05-13 08:21:20 +0545

    learnt about the ownership while passing variables in functions as an argument

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -13,7 +13,35 @@ fn main() {
     println!("s3: {}", s3); //copied
 
     let number1 : u32 = 33;
-    let number2 = number1;
+    let number2 : u32 = number1;
     println!("number1 : {}", number1);
     println!("number2: {}", number2);
+    
+    let s9 : String = String::from("string 9");
+    let s10: String = takes_ownership(s9);      //move
+    println!("string10: {}", s10);
+    // println!("string9: {}, String1-: {}", s9, s10); //s9 is already moved to the funciton so it is not taken valid any longer
+
+    let number_copy : i32 = 12;
+    let result: i32 = makes_copy(number_copy);
+    println!("original number: {}, copied number: {}", number_copy, result);
+
+    //returning multiple values, returning the og string, and something that is manipulated using the og string
+    let original_string : String = String::from("original string");
+    let (og_string, len) = get_orignalstring_and_length(original_string);
+    println!("og_string: {}, length: {}", og_string, len);
+}
+
+fn takes_ownership(s:  String) -> String {
+    return s;
+}
+
+fn makes_copy(number: i32) -> i32{
+    println!("number: {}", number);
+    return number;
+}
+
+fn get_orignalstring_and_length(s: String) -> (String, usize){
+    let length : usize = s.len();
+    return (s, length);
 }
