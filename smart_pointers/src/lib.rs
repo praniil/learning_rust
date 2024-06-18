a1e4018d42d723c26da84a5043bf0067869557b4
praniil <pparajuli15@gmail.com>
2024-06-18 13:38:25 +0545

    learnt about counting the number ownership of a variable

--- smart_pointers/src/lib.rs
+++ smart_pointers/src/lib.rs
@@ -1,3 +1,9 @@
 mod smart_pointer_box;
+mod deref_trait;
+mod drop_trait;
+mod ref_counter_smart_pointer;
 
 pub use smart_pointer_box:: smart_pointer_box;
+pub use deref_trait::deref_traits;
+pub use drop_trait::drop_traits;
+pub use ref_counter_smart_pointer::ref_counter_smart_pointer;
--- smart_pointers/src/main.rs
+++ smart_pointers/src/main.rs
@@ -1,5 +1,12 @@
+use smart_pointers::drop_traits;
+use smart_pointers::ref_counter_smart_pointer;
 use smart_pointers::smart_pointer_box;
+use smart_pointers::deref_traits;
+
 fn main() {
-    println!("Hello, world!");
+    // println!("Hello, world!");
     smart_pointer_box();
+    deref_traits();
+    drop_traits();
+    ref_counter_smart_pointer();
 }
