41aaf97efdf86cb3679a1eed9f010d7f4e57fd3e
praniil <pparajuli15@gmail.com>
2024-09-23 09:31:03 +0545

    used deep copy and move

---
+++ practice/src/ownership.rs
@@ -0,0 +1,16 @@
+pub fn move_ownership() {
+    let s1 = String::from("Hello");
+    let mut s2 = s1;    //move
+    println!("{}", s2);
+    println!("{s2}");
+    s2.push('a');
+    // s1.push('a');
+
+    deep_copy();
+}
+
+fn deep_copy() {
+    let s1 = String::from("Hello Copy");
+    let s2 = s1.clone();    //deep copy, not only stack values are copied but heap's data as well
+    println!("s2: {s2} and s1: {s1}");
+}
